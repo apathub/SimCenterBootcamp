ef12cb5e0f2126efdc7153bd56c9c25a4620864c
Apostolos Athanasiou <32705396+apathub@users.noreply.github.com>
2018-07-31 15:56:10 -0700

    update

--- Code/com_pi_openmp.c
+++ Code/com_pi_openmp.c
@@ -1,26 +1,27 @@
-#include <omp.h>
+
+#define MAX_THREADS 6
+
 #include <stdio.h>
 #include <stdlib.h>
-#define MAX_THREADS 8
+#include <omp.h>
+
 
 static long steps = 1000000000;
 double step;
-
 int main (int argc, const char *argv[]) {
 
     int i,j;
     double x;
     double pi, sum = 0.0;
     double start, delta;
-
     step = 1.0/(double) steps;
 
-    // Compute parallel compute times for 1-MAX_THREADS
+
     for (j=1; j<= MAX_THREADS; j++) {
 
         printf(" running on %d threads: ", j);
 
-        // This is the beginning of a single PI computation 
+
         omp_set_num_threads(j);
 
         sum = 0.0;
@@ -33,7 +34,6 @@ int main (int argc, const char *argv[]) {
             sum += 4.0 / (1.0+x*x); 
         }
 
-        // Out of the parallel region, finialize computation
         pi = step * sum;
         delta = omp_get_wtime() - start;
         printf("PI = %.16g computed in %.4g seconds\n", pi, delta);
